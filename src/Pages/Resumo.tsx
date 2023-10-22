df5a11a57ba17a7af7fe39a5359dd0bd1b4538c3
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2023-10-22 13:15:03 -0300

    add sales summary

--- src/Pages/Resumo.tsx
+++ src/Pages/Resumo.tsx
@@ -2,8 +2,46 @@ import { useData } from '../Context/DataContext';
 
 function Resumo() {
     const { data } = useData();
-    
-    return <div>Resumo</div>;
+
+    if (data === null) return null;
+
+    return (
+        <section>
+            <div className="resumo flex mb">
+                <div className="box">
+                    <h2>Vendas</h2>
+                    {data
+                        .filter(item => item.status !== 'falha')
+                        .reduce((acc, item) => acc + item.preco, 0)
+                        .toLocaleString('pt-br', {
+                            style: 'currency',
+                            currency: 'BRL'
+                        })}
+                </div>
+                <div className="box">
+                    <h2>Recebido</h2>
+                    {data
+                        .filter(item => item.status === 'pago')
+                        .reduce((acc, item) => acc + item.preco, 0)
+                        .toLocaleString('pt-br', {
+                            style: 'currency',
+                            currency: 'BRL'
+                        })}
+                </div>
+                <div className="box">
+                    <h2>Processando</h2>
+                    {data
+                        .filter(item => item.status === 'processando')
+                        .reduce((acc, item) => acc + item.preco, 0)
+                        .toLocaleString('pt-br', {
+                            style: 'currency',
+                            currency: 'BRL'
+                        })}
+                </div>
+            </div>
+            <div className="box">Gráficos</div>
+        </section>
+    );
 }
 
 export default Resumo;
