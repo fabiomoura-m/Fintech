f861cae0d3e0ce8881f310adf08ce69d71b04c4e
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2023-10-23 12:03:54 -0300

    add saleItem component

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import Header from './Components/Header';
 import Sidernav from './Components/Sidenav';
 import { DataContextProvider } from './Context/DataContext';
 import Resumo from './Pages/Resumo';
+import Vendas from './Pages/Vendas';
 import './style.css';
 
 function App() {
     return (
         <DataContextProvider>
-            <div className='container'>
+            <div className="container">
                 <Sidernav />
                 <main>
                     <Header />
                     <Resumo />
+                    <Vendas />
                 </main>
             </div>
         </DataContextProvider>
